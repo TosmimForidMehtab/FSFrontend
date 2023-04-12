ba4c518950dddf86955d8b40a23343d298ed0c63
Tosmim <tosmimforidmehtab@gmail.com>
2023-04-12 20:57:15 +0530

    Using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { Entry } from './Entry';
 import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <div className='h-screen font-serif text-white bg-gray-900 grid place-items-center'>
       {/* <App /> */}
       <Entry />
     </div>
-  </BrowserRouter>
+  </HashRouter>
 
 );
