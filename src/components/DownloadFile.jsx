c9ee3c319df034b5e4556871bdc1e5fc7ebbdcd5
Tosmim <tosmimforidmehtab@gmail.com>
2023-04-12 21:24:16 +0530

    Alert"

--- src/components/DownloadFile.jsx
+++ src/components/DownloadFile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const DownloadFile = ({ downloadPageLink }) => {
     return (
@@ -6,10 +6,10 @@ const DownloadFile = ({ downloadPageLink }) => {
             <h1 className='my-2 text-lg font-medium'>Copy the link and share</h1>
             <div className='flex space-x-3'>
                 <span className='break-all'>{downloadPageLink}</span>
-                <img src="/images/copy.png" alt="" className='w-8 h-8 object-contain cursor-pointer bg-white' onClick={() => navigator.clipboard.writeText(downloadPageLink)} />
+                <img src="/images/copy.png" alt="" className='w-8 h-8 object-contain cursor-pointer bg-white' onClick={() => { navigator.clipboard.writeText(downloadPageLink); alert("Link copied to clipboard"); }} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default DownloadFile
+export default DownloadFile;
