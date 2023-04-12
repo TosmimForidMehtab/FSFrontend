59d1ccd1b71a3c33149a2b376ed730fb813c56b1
Tosmim <tosmimforidmehtab@gmail.com>
2023-04-12 21:17:46 +0530

    Original

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { Entry } from './Entry';
 import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <div className='h-screen font-serif text-white bg-gray-900 grid place-items-center'>
       {/* <App /> */}
       <Entry />
     </div>
-  </HashRouter>
+  </BrowserRouter>
 
 );
